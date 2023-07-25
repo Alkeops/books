d1b1b9d28362e83ff114ca2ca7b976244f026a7e
Manuel <m_adan2012@outlook.com>
2023-07-24 20:17:58 -0500

    clean category products

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -4,27 +4,24 @@ import { getBooks } from "../lib/books.requests";
 import { ItemListContainer } from "../components";
 
 export const Category = () => {
-    const {id} = useParams();
-    const [products, setProducts] = useState([]); 
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-  
-     getBooks(id) 
-      .then(res => {
-        setIsLoading(false); 
-        setProducts(res)} 
-        
-        ) 
-    }, [id]);
-  
-  
-    return (
-      <div>
-        <div className="container">
-          <h5>{isLoading ? "Cargando ..." : "Listo"}</h5>
-          <ItemListContainer products={products} />
-        </div>
-      </div>
-    );
+  const { id } = useParams();
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-}
+  useEffect(() => {
+    setProducts([]);
+    getBooks(id).then((res) => {
+      setIsLoading(false);
+      setProducts(res);
+    });
+  }, [id]);
+
+  return (
+    <div>
+      <div className="container">
+        <h5>{isLoading ? "Cargando ..." : "Listo"}</h5>
+        <ItemListContainer products={products} />
+      </div>
+    </div>
+  );
+};
